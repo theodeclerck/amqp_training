8aa5e931dcdc3258083b987f6115f2f1001e5e17
theo declerck <theo.declerck@epita.com>
2022-01-26 14:48:14 +0100

    Partie 6 - Client AMQP

--- client/src/main/java/fr/lernejo/chat/Launcher.java
+++ client/src/main/java/fr/lernejo/chat/Launcher.java
@@ -1,19 +1,24 @@
 package fr.lernejo.chat;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Scanner;
 
 @SpringBootApplication
 public class Launcher {
     public static void main(String[] args) {
-        SpringApplication.run(Launcher.class, args);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Launcher.class);
+        RabbitTemplate rabbitTemplate = context.getBean(RabbitTemplate.class);
         Scanner entry = new Scanner(System.in);
-        String s;
+        String send;
         do{
-            s = entry.next();
-            System.out.println("Valentin le nulos");
-        }while (!s.equals("q"));
+            send = entry.nextLine();        
+            rabbitTemplate.convertAndSend("chat_message", send);
+            System.out.println("Message sent");
+        }while (!send.equals("q"));
+        context.close();
     }
 }
